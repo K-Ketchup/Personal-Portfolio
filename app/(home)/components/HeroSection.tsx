091ffd06669f8cbf5591f9fcf9f7351f9190b757
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2024-04-02 22:59:43 -0700

    Herosection changes finished

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -2,33 +2,36 @@ import Link from "next/link";
 import React from "react"
 
 export default function HeroSection() {
-    return <div>
-        <div>
-            <h1>
+    return <div className="min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between">
+        <div className="space-y-10 text-center lg:text-left">
+            <h1 className="text-4xl lg:text-7xl font-bold">
                 Nice to meet you! 
-                <br/> <span>{"I'm Kensho."}</span>
+                <br/> <span className="underline underline-offset-8 decoration-green-500">{"I'm Kensho."}</span>
             </h1>
-            <p>
+            <p className="md:w-96 text-lg text-gray-300">
                 {
                     "I'm a highschool developer based in Los Angeles, California. I enjoy creating mobile applications and low-level programs."
                 }
             </p>
-            <Link href={"mailto:kenshonishio7@gmail.com"}>
+            <Link href={"mailto:kenshonishio7@gmail.com"} className="inline-block group">
                 <div>
-                    <h1>Contact Me</h1>
+                    <h1 className="text-3xl font-bold group-hover:text-green-400 transition-all">Contact Me</h1>
+                    <div className="w-40 h-2 bg-green-500 rounded-full"></div>
+                    <div className="w-40 h-2 bg-indigo-500 rounded-full translate-x-2"></div>
                 </div>
             </Link>
         </div>
         <div>
-            <div className="w-72 h-72">
+            <div className="w-72 h-72 space-y-3 -rotate-[30deg] relative">
                 <div className="flex gap-3 translate-x-8">
-                    <div className="w-32 h-32 rounded-2x1 bg-green-500"></div>
+                    <div className="w-32 h-32 rounded-2xl bg-green-500"></div>
                     <div className="w-32 h-32 rounded-full bg-indigo-500"></div>
                 </div>
                 <div className="flex gap-3 -translate-x-8">
-                    <div className="w-32 h-32 rounded-2x1 bg-indigo-500"></div>
+                    <div className="w-32 h-32 rounded-2xl bg-indigo-500"></div>
                     <div className="w-32 h-32 rounded-full bg-green-500"></div>
                 </div>
+                <div className="glow absolute top-[40%] right-1/2 -z-10"></div>
             </div>
         </div>
     </div>;
