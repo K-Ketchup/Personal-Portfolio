48178d7c3eb14ae6e0e1f1a6694c9458fa388565
K_Ketchup <kenshonishio7@gmail.com>
2024-10-31 00:26:57 -0700

    Projects and Skills updates

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -62,13 +62,6 @@ export default function Skills() {
                     <p className="font-normal text-sm">3 Years. Appdev.</p>
                 </DirectionAwareHover>
             </div>
-            <div className="flex justify-center">
-                <Link href="https://github.com/K-Ketchup/Personal-Portfolio">
-                    <button className="transform hover:translate-y-0.5 transition duration-400 flex inline-flex h-12 w-80 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                        Github For This Project 😼
-                    </button>
-                </Link>
-            </div>
         </div>
     );
 }
