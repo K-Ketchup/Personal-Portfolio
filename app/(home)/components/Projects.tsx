0e7b450d037b84061bbb11bc67ebccd302cef8a2
K_Ketchup <kenshonishio7@gmail.com>
2024-10-31 00:05:35 -0700

    Project y adjustmnet

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
     ];
 
     return (
-        <div className="py-10 p-5 sm:p-0">
+        <div className="py-20 p-5 sm:p-0">
             <Title text="Projects" className="flex flex-col items-center justify-center rotate-6"/>
         </div>
     );
