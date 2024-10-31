ee331d2e0d6d3f4ab38c3c151920b84d2c550748
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2024-10-30 23:10:28 -0700

    fixed Title

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -23,11 +23,7 @@ export default function Projects() {
 
     return (
         <div className="py-10 p-5 sm:p-0">
-            <Title
-                text="Projects"
-
-                className="flex flex-col items-center justify-center rotate-6"
-            />
+            <Title text="Projects" className="flex flex-col items-center justify-center rotate-6"/>
         </div>
     );
 }
