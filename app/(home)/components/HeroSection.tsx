9cb2d4b3d141a765871b8c65ac4e8ae27077778b
Kensho Nishio <kensho_nishio@brown.edu>
2026-01-30 23:34:49 -0500

    Fixed new line breaks.

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -19,7 +19,7 @@ export default function HeroSection() {
             </h1>
             <p className="md:w-96 text-lg text-gray-300">
                 {
-                    "I'm a developer and student at Brown University, currently studying Computer Science and Art History.\n\nI enjoy creating mobile applications, video games, and low-level programs. Outside of programming, I am particularly fascinated by ideas surrounding fashion design.\n\nI am currently taking CS300: Fundamental of Computer Systems while working on a personal project focused on machine learning and art."
+                    "I'm a developer and student at Brown University, currently studying Computer Science and Art History.{"\n"}{"\n"}I enjoy creating mobile applications, video games, and low-level programs. Outside of programming, I am particularly fascinated by ideas surrounding fashion design.{"\n"}{"\n"}I am currently taking CS300: Fundamental of Computer Systems while working on a personal project focused on machine learning and art."
                 }
             </p>
 
